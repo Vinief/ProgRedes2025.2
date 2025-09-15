c16e025a0b35ce159d9dfc7c55b35d9eaaf172b6
Vinief <viniaedu314@gmail.com>
2025-09-15 16:28:42 -0300

    Update Euler52.py
    tem que arrumar

--- praticas/Euler52.py
+++ praticas/Euler52.py
@@ -2,15 +2,20 @@ x = '1'
 cond_1 = True
 while cond_1:
     
-    num = str(int(x) * 2)
+    num_2 = str(int(x) * 2)
+    num_3 = str(int(x) * 3)
+    num_4 = str(int(x) * 4)
+    num_5 = str(int(x) * 5)
+    num_6 = str(int(x) * 6)
     
     cont = 0
     
     cond = True
-    if len(num) == len(x):
+    if len(num_6) == len(x):
         
         while cond: 
-            if x[cont] in num and num[cont] in x and x[cont] != num[cont] and x.count(x[cont]) == num.count(x[cont]):
+            if x[cont] in num_2 and num_2[cont] in x and x[cont] != num_2[cont] and x.count(x[cont]) == num_2.count(x[cont]) and x[cont] in num_3 and num_3[cont] in x and x[cont] != num_3[cont] and x.count(x[cont]) == num_3.count(x[cont]) and x[cont] in num_4 and num_4[cont] in x and x[cont] != num_4[cont] and x.count(x[cont]) == num_4.count(x[cont]) and x[cont] in num_5 and num_5[cont] in x and x[cont] != num_5[cont] and x.count(x[cont]) == num_5.count(x[cont]) and x[cont] in num_6 and num_6[cont] in x and x[cont] != num_6[cont] and x.count(x[cont]) == num_6.count(x[cont]):
+
                 cont += 1 
                 if cont == len(x):
                     cond,cond_1 = False,False
@@ -19,6 +24,6 @@ while cond_1:
 
     else:
         x = 10**len(str(x))
-
-    x = str(int(x) + 1)    
-print(x , num)
+    if cond_1:
+        x = str(int(x) + 1)    
+print(x , num_2,num_3,num_4,num_5,num_6)
