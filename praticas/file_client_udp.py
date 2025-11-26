a0296510248f538e7b0853438aa181134f04971e
Vinicius Eduardo <viniaedu314@gmail.com>
2025-11-26 12:30:43 -0300

    Update file_client_udp.py

--- praticas/file_client_udp.py
+++ praticas/file_client_udp.py
@@ -3,8 +3,6 @@ import socket ,os
 host = socket.gethostbyname(socket.getfqdn())
 port = 60000
 
-para = '10.25.3.43'
-
 udp_socket = socket.socket(socket.AF_INET , socket.SOCK_DGRAM)
 udp_socket.bind((host,port))
 
@@ -12,16 +10,24 @@ msg_recebida = ''
 
 while msg_recebida != '!q':
     
-    mgs = input('digite o nome do arquivo:')
+    arquivo = input('digite o nome do arquivo:')
     
     print('enviadando msg...')
-    udp_socket.sendto(mgs.encode('utf-8') , (host,port))
+    udp_socket.sendto(arquivo.encode('utf-8') , (host,port))
     
     print('recebendo msg...')
-    data , src = udp_socket.recvfrom(1024)
+    retorno , src = udp_socket.recvfrom(1)
     
-    msg_recebida = data.decode('utf-8')
-    print(msg_recebida)
+    if int.from_bytes(retorno) != 0:
+        
+        data , src = udp_socket.recvfrom(4)
+        udp_socket.recvfrom(int.from_bytes(data))
+        f = open(arquivo,'wb')
+        f.write(data)
+        f.close()
+
+    else:
+        print('arquivo n existe')
 
 udp_socket.close()
 
