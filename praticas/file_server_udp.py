1dd4ad500b7aa5266844e106f673645999d0e81f
Vinicius Eduardo <viniaedu314@gmail.com>
2025-11-28 12:15:52 -0300

    Update file_server_udp.py

--- praticas/file_server_udp.py
+++ praticas/file_server_udp.py
@@ -14,22 +14,36 @@ while True:
     try:
         #abre arquivo
         f  = open(nome_arquivo.decode('utf-8'),'rb')
-        size = os.path.getsize(nome_arquivo.decode('utf-8'))
+        tamanho = os.path.getsize(nome_arquivo.decode('utf-8'))
         
         #byte de aviso
         udp_socket.sendto((5).to_bytes(1,'big') , src)
         print(f'enviei isso {(5).to_bytes(1,'big')} por essa porta e ip {src[0],port}')
         
         #tamanho do arquivo
-        udp_socket.sendto(size.to_bytes(4 ,'big'), src)
-        print(f'enviei isso {int.from_bytes(size.to_bytes(4,'big'))} por essa porta e ip {src[0],port}')
+        udp_socket.sendto(tamanho.to_bytes(4 ,'big'), src)
+        print(f'enviei isso {int.from_bytes(tamanho.to_bytes(4,'big'))} por essa porta e ip {src[0],port}')
         
         #lendo e enviando o arquivo
-        arquivo = f.read(size)
-        udp_socket.sendto(arquivo , src)
-        print(f'enviei isso {arquivo} por essa porta e ip {src[0],port}')
+        if 4096 >= tamanho: 
+            arquivo = f.read(tamanho)
+            udp_socket.sendto(arquivo , src)
+            print(f'enviei isso {arquivo} por essa porta e ip {src[0],port}')
+            
+        else:
+            while tamanho > 0:
+                if tamanho > 4096:
+                    pacote = 4096
+                    arquivo = f.read(pacote)
+                    udp_socket.sendto(arquivo , src)
+                    tamanho -= pacote
+                else:
+                    arquivo = f.read(tamanho)
+                    udp_socket.sendto(arquivo , src)
+                    tamanho = 0
+                print(f'enviei isso {arquivo} por essa porta e ip {src[0],port}')
+        
         f.close()
-
     except FileNotFoundError:
         udp_socket.sendto((0).to_bytes(1,'big') , src)
     
