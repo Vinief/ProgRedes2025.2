2384ad18e4e6da12b77f35000bdff58446a1a9c9
Vinicius Eduardo <viniaedu314@gmail.com>
2025-12-01 17:45:47 -0300

    Update file_server_udp.py

--- praticas/file_server_udp.py
+++ praticas/file_server_udp.py
@@ -1,4 +1,4 @@
-import socket, os
+import socket, os,time
 
 host = ''
 port = 20000
@@ -12,10 +12,10 @@ while nome_arquivo_decode != '!q':
     print(f'esperando resposta do cliente...')
     #recebe tam do nome
     nome_tam , src = udp_socket.recvfrom(1)
-    print(f'recebi isso:{int.from_bytes(nome_tam)} desse ip e porta {src}')
+    print(f'recebi isso:{int.from_bytes(nome_tam,byteorder = "big")} desse ip e porta {src}')
     
     #recebe nome
-    nome_arquivo , src = udp_socket.recvfrom(int.from_bytes(nome_tam))
+    nome_arquivo , src = udp_socket.recvfrom(int.from_bytes(nome_tam,byteorder = "big"))
     nome_arquivo_decode = nome_arquivo.decode('utf-8')
     print(f'recebi isso:{nome_arquivo_decode} desse ip e porta {src}')
     
@@ -25,13 +25,13 @@ while nome_arquivo_decode != '!q':
         tamanho = os.path.getsize(nome_arquivo.decode('utf-8'))
         
         #byte de aviso
-        existe = (1).to_bytes(1,'big') 
+        existe = (1).to_bytes(1,"big") 
         udp_socket.sendto(existe , src)
-        print(f'enviei isso {int.from_bytes(existe)} por essa porta e ip {src}')
+        print(f'enviei isso {int.from_bytes(existe, byteorder = "big")} por essa porta e ip {src}')
         
         #tamanho do arquivo
-        udp_socket.sendto(tamanho.to_bytes(4 ,'big'), src)
-        print(f'enviei isso {int.from_bytes(tamanho.to_bytes(4,'big'))} por essa porta e ip {src}')
+        udp_socket.sendto(tamanho.to_bytes(4 ,"big"), src)
+        print(f'enviei isso {int.from_bytes(tamanho.to_bytes(4,"big"),byteorder = "big")} por essa porta e ip {src}')
         
         #lendo e enviando o arquivo
         if 4096 >= tamanho: 
@@ -45,6 +45,7 @@ while nome_arquivo_decode != '!q':
                     pacote = 4096
                     arquivo = f.read(pacote)
                     udp_socket.sendto(arquivo , src)
+                    time.sleep(0.1)
                     tamanho -= pacote
                     print(f.tell())
                 else:
@@ -58,11 +59,9 @@ while nome_arquivo_decode != '!q':
     except FileNotFoundError:
         #caso o arquivo n exista
         if nome_arquivo_decode != '!q':
-            existe = (0).to_bytes(1,'big')
-            udp_socket.sendto((0).to_bytes(1,'big') , src)
+            existe = (0).to_bytes(1,"big")
+            udp_socket.sendto((0).to_bytes(1,"big") , src)
             print(f'enviei isso {existe} por essa porta e ip {src}')
         else:print('programa encerrado!!!')
-    except:
-        print('houve algum erro')
 
 udp_socket.close()
