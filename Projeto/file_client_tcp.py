0deb818de45ed0dc04381c650ec0ab11f4b743db
Vinicius Eduardo <viniaedu314@gmail.com>
2025-12-11 01:44:30 -0300

    Update file_client_tcp.py

--- Projeto/file_client_tcp.py
+++ Projeto/file_client_tcp.py
@@ -5,76 +5,88 @@ port = 20000
 
 tcp_socket = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
 tcp_socket.connect((host, port))
+encerra_prog = False
 
-nome_arquivo = ''
 
-while nome_arquivo != '!q':
-
-    option = int(input('digite a opção '))
-    tcp_socket.send(option.to_bytes(1, "big"))
+while not encerra_prog:
+    try:
+        option = int(input('digite a opção:'))
+        tcp_socket.send(option.to_bytes(1, "big"))
     
-    if option == 10:
-        nome_arquivo = input('digite o nome do arquivo:')    
-        nome_arquivo_encode = nome_arquivo.encode('utf-8')
-        nome_tam = len(nome_arquivo).to_bytes(4 , byteorder = 'big')
-        
-        funcoes.send(tcp_socket, nome_tam, nome_arquivo_encode)
-
-        status = int.from_bytes(tcp_socket.recv(1), "big")
-        
-        if status != 0:
-            f = open(f"../STORAGE_CLIENT/{nome_arquivo}" ,'wb')
-
-            dados = funcoes.recv(tcp_socket)
-            print('arquivo recebido com sucesso!!!')
-
-
-            f.write(dados)
-            f.close()
-        else:
-            if nome_arquivo != '!q':
-                print('arquivo n existe')
-            else:print('programa encerrado!!!')
-    if option == 20:
-        status = int.from_bytes(tcp_socket.recv(1), "big")
-    
-        if status != 0:
+        if option == 10:
+            nome_arquivo = input('digite o nome do arquivo:')    
+            nome_arquivo_encode = nome_arquivo.encode('utf-8')
+            nome_tam = len(nome_arquivo).to_bytes(4 , byteorder = 'big')
             
-            dados = funcoes.recv(tcp_socket)
-            print('arquivo recebido com sucesso!!!')
+            funcoes.send(tcp_socket, nome_tam, nome_arquivo_encode)
+            print('nome do arquivo enviado com sucesso!!!')
 
-            json = dados.decode('utf-8')
-            funcoes.le_json(json)
-
-        else:
-            print('houve algum erro')
-
-    if option == 30:
-        enviou_tudo = False
-        while not enviou_tudo:
-            try:
-                nome_arquivo = input('digite o nome do arquivo que voce deseja upar:')
-                f = open(f'../STORAGE_CLIENT/{nome_arquivo}', 'rb')
-                nome_arquivo_encode = nome_arquivo.encode('utf-8')
-                nome_tam = len(nome_arquivo).to_bytes(4, "big")
-
-                funcoes.send(tcp_socket, nome_tam, nome_arquivo_encode)
-
-                status = int.from_bytes(tcp_socket.recv(1), "big")
-
-                if status == 1:
-                    
-                    tamanho = os.path.getsize(f'../STORAGE_CLIENT/{nome_arquivo}').to_bytes(4, "big")
-                    dado = f.read()
-
-                    funcoes.send(tcp_socket, tamanho, dado)
-                    print('arquivo enviado com sucesso!!!')
-                    f.close()
-
-                    enviou_tudo = True
-
-            except FileNotFoundError:
-                print('digite o nome de um arquivo que existe!!!')
+            status = int.from_bytes(tcp_socket.recv(1), "big")
             
+            if status != 0:
+                f = open(f"../STORAGE_CLIENT/{nome_arquivo}" ,'wb')
+
+                dados = funcoes.recv(tcp_socket)
+                print('arquivo recebido com sucesso!!!')
+
+
+                f.write(dados)
+                f.close()
+
+                
+        if option == 20:
+            status = int.from_bytes(tcp_socket.recv(1), "big")
+        
+            if status != 0:
+                
+                dados = funcoes.recv(tcp_socket)
+                print('arquivo recebido com sucesso!!!')
+
+                json = dados.decode('utf-8')
+                funcoes.le_json(json)
+
+            else:
+                print('houve algum erro')
+
+        if option == 30:
+            enviou_tudo = False
+            while not enviou_tudo:
+                try:
+                    nome_arquivo = input('digite o nome do arquivo que voce deseja upar:')
+                    f = open(f'../STORAGE_CLIENT/{nome_arquivo}', 'rb')
+                    nome_arquivo_encode = nome_arquivo.encode('utf-8')
+                    nome_tam = len(nome_arquivo).to_bytes(4, "big")
+
+                    funcoes.send(tcp_socket, nome_tam, nome_arquivo_encode)
+                    print('nome do arquivo enviado com sucesso')
+
+                    status = int.from_bytes(tcp_socket.recv(1), "big")
+
+                    if status == 1:
+                        
+                        tamanho = os.path.getsize(f'../STORAGE_CLIENT/{nome_arquivo}').to_bytes(4, "big")
+                        dado = f.read()
+
+                        funcoes.send(tcp_socket, tamanho, dado)
+                        print('arquivo enviado com sucesso!!!')
+                        f.close()
+
+                        enviou_tudo = True
+
+                except FileNotFoundError:
+                    print('digite o nome de um arquivo que existe!!!')
+        
+        if option == 60:
+            print('programa encerrado com sucesso!!!')
+            encerra_prog = True 
+        else:
+            print('essa opcao nao existe!!!')
+        
+        
+    except ValueError:
+        print('digite um numero inteiro!!!')
+    except:
+        print('houve algum erro')
+
 
 tcp_socket.close()
